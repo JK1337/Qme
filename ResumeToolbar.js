451be4585775a345fe3bf7f2cae51c4207476c29
Jasper Kindt <JK1337@users.noreply.github.com>
2026-03-25 14:33:57 +0100

    Add ResumeToolbar component

---
+++ ResumeToolbar.js
@@ -0,0 +1,14 @@
+// ResumeToolbar Component
+
+import React from 'react';
+
+const ResumeToolbar = () => {
+    return (
+        <div className="resume-toolbar">
+            <button className="save-button">Save</button>
+            <button className="share-button">Share</button>
+        </div>
+    );
+};
+
+export default ResumeToolbar;
